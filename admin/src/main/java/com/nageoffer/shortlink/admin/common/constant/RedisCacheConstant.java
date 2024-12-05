447988f6ddaf9051978d4966d1f8510bced094d3
WangYan-collb <1552096275@qq.com>
2024-12-05 21:43:56 +0800

    feature:通过分布式锁完善用户注册功能，防止恶意请求短时间内使用同一未注册用户名

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,10 @@
+package com.nageoffer.shortlink.admin.common.constant;
+
+/**
+ * 短链接后管 redis 缓存常量类
+ */
+public class RedisCacheConstant {
+    public static final String LOCK_USER_REGISTER_KEY="short-link:lock_user-register:";
+
+
+}
