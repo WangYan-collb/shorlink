23dba930437f0f4679298e1a4dfac425cc37001e
WangYan-collb <1552096275@qq.com>
2024-12-18 16:32:17 +0800

    feature:通过分布式锁完善用户注册功能，防止恶意请求短时间内使用同一未注册用户名

--- admin/src/main/java/com/nageoffer/shortlink/admin/config/RBloomFilterConfiguration.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/config/RBloomFilterConfiguration.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.nageoffer.shortlink.admin.config;
 
 import org.redisson.api.RBloomFilter;
@@ -7,8 +24,9 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * 布隆过滤器配置
+ * 公众号：马丁玩编程，回复：加群，添加马哥微信（备注：link）获取项目资料
  */
-@Configuration
+@Configuration(value = "rBloomFilterConfigurationByAdmin")
 public class RBloomFilterConfiguration {
 
     /**
@@ -21,6 +39,10 @@ public class RBloomFilterConfiguration {
         return cachePenetrationBloomFilter;
     }
 
+    /**
+     * 防止分组标识注册查询数据库的布隆过滤器
+     */
+    @Bean
     public RBloomFilter<String> gidRegisterCachePenetrationBloomFilter(RedissonClient redissonClient) {
         RBloomFilter<String> cachePenetrationBloomFilter = redissonClient.getBloomFilter("gidRegisterCachePenetrationBloomFilter");
         cachePenetrationBloomFilter.tryInit(200000000L, 0.001);
--- admin/src/main/java/com/nageoffer/shortlink/admin/dao/entity/UserDO.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/dao/entity/UserDO.java
@@ -1,31 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.nageoffer.shortlink.admin.dao.entity;
 
-import com.baomidou.mybatisplus.annotation.*;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.nageoffer.shortlink.admin.common.database.BaseDO;
 import lombok.Data;
 
-import java.util.Date;
-/*
-* 用户持久层实体
-*
-* */
+/**
+ * 用户持久层实体
+ * 公众号：马丁玩编程，回复：加群，添加马哥微信（备注：link）获取项目资料
+ */
 @Data
 @TableName("t_user")
-public class UserDO {
-    private static final long serialVersionUID = 1L;
+public class UserDO extends BaseDO {
 
-    @TableId(type = IdType.AUTO)
     /**
-     * IDr
+     * id
      */
     private Long id;
 
     /**
-     * 用户名r
+     * 用户名
      */
     private String username;
 
     /**
-     * 密码r
+     * 密码
      */
     private String password;
 
@@ -40,7 +55,7 @@ public class UserDO {
     private String phone;
 
     /**
-     * 邮箱号
+     * 邮箱
      */
     private String mail;
 
@@ -48,22 +63,4 @@ public class UserDO {
      * 注销时间戳
      */
     private Long deletionTime;
-
-    /**
-     * 创建时间
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private Date createTime;
-
-    /**
-     * 修改时间
-     */
-    @TableField(fill = FieldFill.INSERT_UPDATE)
-    private Date updateTime;
-
-    /**
-     * 删除标识0：未删除rn1：已删除
-     */
-    @TableField(fill = FieldFill.INSERT)
-    private Integer delFlag;
 }
